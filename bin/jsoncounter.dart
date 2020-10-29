5dd491c35d62eb0ff69231db2309e9d64028b485
Tamas Kovacs <tk@delodi.net>
2020-10-29 13:21:18 +0100

    console changes

--- bin/jsoncounter.dart
+++ bin/jsoncounter.dart
@@ -1,11 +1,24 @@
+import 'package:args/args.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
 import 'package:jsoncounter/jsoncounter.dart' as jsoncounter;
 
 void main(List<String> arguments) {
-  var data = '[{"alma" : 5},1,2, "alma", 0.4444]';
-  data =
-      '{"bela":["taken",true,"its",1,-693025855.9177513,[{"upon":-796907803,"slabs":{"heard":794231106.691812,"fallen":789968425.6181083,"gift":1977191675.6533527,"aloud":"sugar","space":910085137.3047051},"union":{"sick":442982456.54306936,"solid":false,"kept":false,"ate":false,"species":-2069521999.5737042},"model":false,"brick":-1819117779},true,1689174800,"remove","passage"]],"alma":[-616580849.5894508,"now",true,[-1226230815.7943583,["judge","fence",{"satisfied":"fewer","negative":"sell","calm":true,"aid":-58416027.80156088,"whatever":false},"magnet","lady"],true,false,1495953974],true]}';
+  final parser = ArgParser();
 
-  // data = '[{"alma" : 5},1,2, "alma", 0.4444';
+  parser.addOption(
+    'file',
+    help: 'The path of the file',
+    abbr: 'f',
+    defaultsTo: '/Users/tamas/Work/Private/jsoncounter/test/test.json',
+  );
 
-  jsoncounter.parseAndSum(data);
+  final argResults = parser.parse(arguments);
+
+  File(argResults['file']).readAsString().then((String contents) {
+    jsoncounter.parseAndSum(contents);
+  }).catchError((e) {
+    print(e.toString());
+  });
 }
